23e469870603bda75b232aabb9754cbcba428755
Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>
2024-07-03 16:23:04 +0200

    Implement option to automatically select public IP address
    Signed-off-by: Dominik Prodinger <35099504+prodingerd@users.noreply.github.com>

---
+++ core/ifconfig.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"errors"
+	"io"
+	"net"
+	"net/http"
+)
+
+const (
+	serviceUrl = "https://ifconfig.me"
+)
+
+func GetPublicIp() (net.IP, error) {
+	response, err := http.Get(serviceUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ip := net.ParseIP(string(body))
+
+	if ip == nil || ip.To4() == nil {
+		return nil, errors.New("could not automatically determine IPv4 address")
+	}
+
+	return ip, nil
+}
