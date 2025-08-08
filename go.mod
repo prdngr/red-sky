b6bd3a5298417658b4eb762ea780e0e7b17a9988
Dominik Prodinger <35099504+prdngr@users.noreply.github.com>
2025-08-08 15:10:36 +0200

    Upgrade to AWS Go v2 SDK
    Signed-off-by: Dominik Prodinger <35099504+prdngr@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.23.4
 
 require (
 	github.com/adrg/xdg v0.5.3
-	github.com/aws/aws-sdk-go v1.55.8
+	github.com/aws/aws-sdk-go-v2 v1.37.2
+	github.com/aws/aws-sdk-go-v2/config v1.30.3
+	github.com/aws/aws-sdk-go-v2/service/iam v1.45.0
+	github.com/aws/aws-sdk-go-v2/service/sts v1.36.0
 	github.com/briandowns/spinner v1.23.2
 	github.com/fatih/color v1.18.0
 	github.com/google/uuid v1.6.0
@@ -17,6 +20,16 @@ require (
 require (
 	github.com/ProtonMail/go-crypto v1.3.0 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.18.3 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.27.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.32.0 // indirect
+	github.com/aws/smithy-go v1.22.5 // indirect
 	github.com/cloudflare/circl v1.6.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -24,7 +37,6 @@ require (
 	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
 	github.com/hashicorp/terraform-json v0.25.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/pflag v1.0.7 // indirect
--- internal/aws.go
+++ internal/aws.go
@@ -2,48 +2,55 @@ package internal
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/iam"
-	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	v2iam "github.com/aws/aws-sdk-go-v2/service/iam"
+	v2sts "github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 func InitializeAwsSession(profile string) {
 	StartSpinner("Initializing AWS session")
 
-	session, err := session.NewSessionWithOptions(session.Options{Profile: profile})
-
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithSharedConfigProfile(profile))
 	if err != nil {
-		log.Fatalf("error initializing AWS session: %s", err)
+		log.Fatalf("error loading AWS config: %s", err)
 	}
 
-	stsClient := sts.New(session)
-	identity, err := stsClient.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	stsClient := v2sts.NewFromConfig(cfg)
+	identity, err := stsClient.GetCallerIdentity(context.Background(), &v2sts.GetCallerIdentityInput{})
 
 	if err != nil {
 		log.Fatalf("error retrieving AWS account details: %s", err)
 	}
 
 	var accountAlias = "n/a"
-	iamClient := iam.New(session)
-	aliasOutput, err := iamClient.ListAccountAliases(&iam.ListAccountAliasesInput{})
+	iamClient := v2iam.NewFromConfig(cfg)
+	paginator := v2iam.NewListAccountAliasesPaginator(iamClient, &v2iam.ListAccountAliasesInput{})
 
-	if err != nil {
-		log.Fatalf("error retrieving AWS account alias: %s", err)
-	} else if len(aliasOutput.AccountAliases) > 0 {
-		accountAlias = *aliasOutput.AccountAliases[0]
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(context.Background())
+		if err != nil {
+			log.Fatalf("error retrieving AWS account alias: %s", err)
+		}
+
+		if len(page.AccountAliases) > 0 {
+			accountAlias = page.AccountAliases[0]
+			break
+		}
 	}
 
 	StopSpinner("AWS session initialized")
 
 	PrintHeader("AWS Account Information")
 
-	fmt.Printf("▶ Account: %s (%s)\n", *identity.Account, accountAlias)
-	fmt.Printf("▶ Caller ARN: %s\n", *identity.Arn)
-	fmt.Printf("▶ User ID: %s\n", *identity.UserId)
+	fmt.Printf("▶ Account: %s (%s)\n", aws.ToString(identity.Account), accountAlias)
+	fmt.Printf("▶ Caller ARN: %s\n", aws.ToString(identity.Arn))
+	fmt.Printf("▶ User ID: %s\n", aws.ToString(identity.UserId))
 
 	fmt.Print("\nPress Enter to continue...")
 	bufio.NewReader(os.Stdin).ReadString('\n')
